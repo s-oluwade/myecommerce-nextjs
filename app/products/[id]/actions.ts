fd3893d38c664c2ed5b822cc5c620153095ae2aa
Zeus <bamiwade@gmail.com>
2023-08-09 19:46:27 -0500

    several changes

---
+++ app/products/[id]/actions.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { createCart, getCart } from "@/lib/db/cart";
+import { prisma } from "@/lib/db/prisma";
+import { revalidatePath } from "next/cache";
+
+// Note: at the time of implementation, there was a bug in Next.js
+// that causes page navigation to freeze while the server action is executing.
+export async function incrementProductQuantity(productId: string) {
+  const cart = (await getCart()) ?? (await createCart());
+
+  const articleInCart = cart.items.find((item) => item.productId === productId);
+
+  if (articleInCart) {
+    await prisma.cart.update({
+      where: { id: cart.id },
+      data: {
+        items: {
+          update: {
+            where: { id: articleInCart.id },
+            data: { quantity: { increment: 1 } },
+          },
+        },
+      },
+    });
+  } else {
+    await prisma.cart.update({
+      where: { id: cart.id },
+      data: {
+        items: {
+          create: {
+            productId,
+            quantity: 1,
+          },
+        },
+      },
+    });
+  }
+
+  revalidatePath("/products/[id]");
+}
